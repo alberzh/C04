db602ffd76e6025af0cef561328aa2e1df99cb2c
Alberto Zaragoza <alberzh@gmail.com>
2025-07-29 16:14:13 +0200

    Ejercicio ex01 Listo.

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/29 15:41:39 by azaragoz          #+#    #+#             */
+/*   Updated: 2025/07/29 16:10:24 by azaragoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/* int	main(void)
+{
+	char *string = "Esto es una cadena de texto de prueba.";
+	ft_putstr(string);
+	return (0);
+} */
