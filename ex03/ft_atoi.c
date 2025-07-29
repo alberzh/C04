7c454315fd96dd78f3c149037a62de0d97ced62d
Alberto Zaragoza <alberzh@gmail.com>
2025-07-30 01:03:13 +0200

    Ejercicio ex03 Listo.

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alber <alber@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:53:12 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/29 18:17:05 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/30 01:01:50 by alber            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@ int ft_atoi(char *str)
 {
     int i;
     int neg;
+    int result;
 
     i = 0;
+    result = 0;
     while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
         || str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
         {
@@ -25,11 +27,32 @@ int ft_atoi(char *str)
     neg = 0;
     if (str[i] == '+')
         i++;
-    else (str[i] == '-')
+    else if (str[i] == '-')
     {
         neg = 1;
         i++;
     }
-
-    
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    if (neg == 1)
+        return (-result);
+    return (result);
 }
+
+#include <stdio.h>
+
+/* int main(void)
+{
+    printf("ft_atoi(\"42\"): %d\n", ft_atoi("42"));
+    printf("ft_atoi(\"-42\"): %d\n", ft_atoi("-42"));
+    printf("ft_atoi(\"+42\"): %d\n", ft_atoi("+42"));
+    printf("ft_atoi(\"   42\"): %d\n", ft_atoi("   42"));
+    printf("ft_atoi(\"\\t\\v42\"): %d\n", ft_atoi("\t\v42"));
+    printf("ft_atoi(\"42abc\"): %d\n", ft_atoi("42abc"));
+    printf("ft_atoi(\"\"): %d\n", ft_atoi(""));
+    printf("ft_atoi(\"   -123\"): %d\n", ft_atoi("   -123"));
+    return (0);
+} */
