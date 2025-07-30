ba1c73429f81ea8a733ace2df9c85c960e98f7c5
Alberto Zaragoza <alberzh@gmail.com>
2025-07-30 10:49:55 +0200

    Corrección ex03 por función "demasiado larga".

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -3,56 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alber <alber@student.42.fr>                +#+  +:+       +#+        */
+/*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:53:12 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/30 01:01:50 by alber            ###   ########.fr       */
+/*   Updated: 2025/07/30 10:49:27 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int i;
-    int neg;
-    int result;
+	int	i;
+	int	neg;
+	int	result;
 
-    i = 0;
-    result = 0;
-    while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
-        || str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
-        {
-            i++;
-        }
-
-    neg = 0;
-    if (str[i] == '+')
-        i++;
-    else if (str[i] == '-')
-    {
-        neg = 1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    if (neg == 1)
-        return (-result);
-    return (result);
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r'
+		|| str[i] == '\t' || str[i] == '\v')
+		i++;
+	neg = 0;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		neg = 1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	if (neg == 1)
+		return (-result);
+	return (result);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
 
-/* int main(void)
+int	main(void)
 {
-    printf("ft_atoi(\"42\"): %d\n", ft_atoi("42"));
-    printf("ft_atoi(\"-42\"): %d\n", ft_atoi("-42"));
-    printf("ft_atoi(\"+42\"): %d\n", ft_atoi("+42"));
-    printf("ft_atoi(\"   42\"): %d\n", ft_atoi("   42"));
-    printf("ft_atoi(\"\\t\\v42\"): %d\n", ft_atoi("\t\v42"));
-    printf("ft_atoi(\"42abc\"): %d\n", ft_atoi("42abc"));
-    printf("ft_atoi(\"\"): %d\n", ft_atoi(""));
-    printf("ft_atoi(\"   -123\"): %d\n", ft_atoi("   -123"));
-    return (0);
+	printf("ft_atoi(\"42\"): %d\n", ft_atoi("42"));
+	printf("ft_atoi(\"-42\"): %d\n", ft_atoi("-42"));
+	printf("ft_atoi(\"+42\"): %d\n", ft_atoi("+42"));
+	printf("ft_atoi(\"   42\"): %d\n", ft_atoi("   42"));
+	printf("ft_atoi(\"\\t\\v42\"): %d\n", ft_atoi("\t\v42"));
+	printf("ft_atoi(\"42abc\"): %d\n", ft_atoi("42abc"));
+	printf("ft_atoi(\"\"): %d\n", ft_atoi(""));
+	printf("ft_atoi(\"   -123\"): %d\n", ft_atoi("   -123"));
+	return (0);
 } */
