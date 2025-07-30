972de472f8dc276a6e690fcbeed800db072caaf7
Alberto Zaragoza <alberzh@gmail.com>
2025-07-30 15:26:04 +0200

    Corregidos ex02 y ex03.

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:53:12 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/30 10:49:27 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/30 15:25:29 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,11 @@ int	ft_atoi(char *str)
 	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r'
 		|| str[i] == '\t' || str[i] == '\v')
 		i++;
-	neg = 0;
-	if (str[i] == '+')
-		i++;
-	else if (str[i] == '-')
+	neg = -1;
+	while (str[i] == '+' || str[i] == '-')
 	{
-		neg = 1;
+		if (str[i] == '-')
+			neg = neg * -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
@@ -44,12 +43,12 @@ int	ft_atoi(char *str)
 int	main(void)
 {
 	printf("ft_atoi(\"42\"): %d\n", ft_atoi("42"));
-	printf("ft_atoi(\"-42\"): %d\n", ft_atoi("-42"));
+	printf("ft_atoi(\"  -42\"): %d\n", ft_atoi("  -42"));
 	printf("ft_atoi(\"+42\"): %d\n", ft_atoi("+42"));
 	printf("ft_atoi(\"   42\"): %d\n", ft_atoi("   42"));
-	printf("ft_atoi(\"\\t\\v42\"): %d\n", ft_atoi("\t\v42"));
+	printf("ft_atoi(\"\\t   \\v42\"): %d\n", ft_atoi("\t   \v42"));
 	printf("ft_atoi(\"42abc\"): %d\n", ft_atoi("42abc"));
 	printf("ft_atoi(\"\"): %d\n", ft_atoi(""));
-	printf("ft_atoi(\"   -123\"): %d\n", ft_atoi("   -123"));
+	printf("ft_atoi(\" ---+--+1234ab567\"): %d\n", ft_atoi(" ---+--+1234ab567"));
 	return (0);
 } */
